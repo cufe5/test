a175f493ed3b398a6042d833fd4db30a81c90405
iscat <2373645779@qq.com>
2023-03-13 13:48:04 +0800

    first commit

---
+++ com.yujian/DishFlavorServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yujian.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yujian.reggie.dto.DishDto;
+import com.yujian.reggie.entity.DishFlavor;
+import com.yujian.reggie.mapper.DishFlavorMapper;
+import com.yujian.reggie.service.DishFlavorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+
+
+
+}
